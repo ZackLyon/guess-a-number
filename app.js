53a46b38b7fa778dc6a84ed00099f9b7c0378d15
Zack Lyon <zack.j.lyon@gmail.com>
2021-08-17 16:50:44 -0700

    added total victories and defeats counters

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions and grab DOM elements
-import { compareNumbers, displayWin, displayLose, evaluateDecrement, displayGuessesRemaining } from "./util.js";
+import { compareNumbers, clearInput, displayWin, displayLose, displayReset, displayVictories, displayDefeats, evaluateDecrement, displayGuessesRemaining } from './util.js';
 // initialize global state
 
 // set event listeners 
@@ -13,34 +13,44 @@ const userGuess = document.querySelector('#user-guess');
 
 let correctAnswer = randomNumber();
 let numOfGuesses = 4;
+let totalVictories = 0;
+let totalDefeats = 0;
+
 console.log(correctAnswer);
 
 subButton.addEventListener('click', () => {
+    
     let newGuess = Number(userGuess.value);
     let correctHighOrLow = compareNumbers(newGuess, correctAnswer); //will guess be correct, high or low
     if (correctHighOrLow === 0) { //guess is correct
-        displayWin();
         subButton.disabled = true;
+        displayWin();
+        totalVictories++;
+        displayVictories(totalVictories);
+        clearInput();
         return;
     }
 
-    numOfGuesses = evaluateDecrement(correctHighOrLow, numOfGuesses); //guess is high or low, respond accordingly
+    numOfGuesses = evaluateDecrement(correctHighOrLow, numOfGuesses); //guess is high or low, decrement guesses
     displayGuessesRemaining(numOfGuesses);
 
     if (numOfGuesses <= 0) { //out of guesses
         subButton.disabled = true;
         displayLose();
+        totalDefeats++;
+        displayDefeats(totalDefeats);
     }
+    clearInput();
 });
 
-numOfGuesses <= 0 ? subButton.disabled = true : subButton.disabled = false;
-
 resetButton.addEventListener('click', () => {
+    clearInput();
     correctAnswer = randomNumber();
     console.log(correctAnswer);
     numOfGuesses = 4;
     displayGuessesRemaining(numOfGuesses);
     subButton.disabled = false;
+    displayReset();
 });
 
 function randomNumber() {
--- util.js
+++ util.js
@@ -1,14 +1,20 @@
-export { compareNumbers, displayWin, displayLose, evaluateDecrement, displayGuessesRemaining };
+export { compareNumbers, clearInput, displayWin, displayLose, displayReset, displayVictories, displayDefeats, evaluateDecrement, displayGuessesRemaining };
 
 const outcomeEl = document.querySelector('#outcome');
 const evaluationEl = document.querySelector('#display-evaluation');
 const guessesRemainingEl = document.querySelector('#guesses-remaining');
+const victoriesEl = document.querySelector('#victories');
+const defeatsEl = document.querySelector('#defeats');
+const inputEl = document.querySelector('#user-guess');
 
 function compareNumbers(guess, correctNumber) {
     if (guess === correctNumber) return 0;
     return guess > correctNumber ? 1 : -1;
 }
 
+function clearInput() {
+    inputEl.value = '';
+}
 function displayWin() {
     outcomeEl.textContent = 'Congratulations! You guessed correctly!';
     outcomeEl.classList.remove('hidden');
@@ -17,6 +23,19 @@ function displayWin() {
 function displayLose() {
     outcomeEl.textContent = 'Game Over!';
     outcomeEl.classList.remove('hidden');
+}
+
+function displayReset() {
+    outcomeEl.classList.add('hidden');
+}
+
+function displayVictories(victoryCount) {
+    victoriesEl.textContent = victoryCount;
+    // console.log(typeof victoryCount);
+}
+
+function displayDefeats(defeatCount) {
+    defeatsEl.textContent = defeatCount;
 }
 
 function evaluateDecrement(evalNum, counter) {
